13e61497bce6a35918784e0854014fa67e567221
MeijerM1 <max1_meijer@hotmail.com>
2019-09-22 17:00:45 +0200

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'tabs',
+        loadChildren: './tabs/tabs.module#TabsPageModule'
+      },
+      {
+        path: 'four',
+        loadChildren: './four/four.module#FourPageModule'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'tabs/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'list',
+        loadChildren: () => import('../list/list.module').then(m => m.ListPageModule)
+      },
+      {
+        path: 'three',
+        loadChildren: () => import('../three/three.module').then(m => m.ThreePageModule)
+      },
+    ]
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule { }
